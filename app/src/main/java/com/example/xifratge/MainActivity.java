5d0e7262a8732dd819694cab1fd909cc1a5661a0
Gastón Rossi <gaston.rossi@basetis.com>
2019-11-06 14:46:53 +0100

    RSA

--- app/src/main/java/com/example/xifratge/MainActivity.java
+++ app/src/main/java/com/example/xifratge/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.xifratge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button decodeButton;
+    private TextView textField;
+    private TextView textEncoded;
+    private TextView textDecoded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textField = findViewById(R.id.editText);
+        this.textEncoded = findViewById(R.id.editText);
+        this.textDecoded = findViewById(R.id.textView);
+        this.decodeButton = findViewById(R.id.button);
+
     }
 }
