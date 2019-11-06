5356f870ac69650000f1165c7a3041f84509c395
Gastón Rossi <gaston.rossi@basetis.com>
2019-11-06 15:21:07 +0100

    Working para un 8

--- app/src/main/java/com/example/xifratge/MainActivity.java
+++ app/src/main/java/com/example/xifratge/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.xifratge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.*;
 import android.widget.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.RandomAccessFile;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,12 +66,72 @@ public class MainActivity extends AppCompatActivity {
 
             decodedText = decodeRsa.Decrypt(encodedText);
             textEncoded.setText(decodedText);
-
+            writeToFile(createXML(password, encodedText) ,getApplicationContext());
 
         } catch (Exception e) {
             System.out.println("An error ocurred encrypting the password");
         }
+    }
 
+        public String createXML(String userText, String userTextCrypt) throws
+        IOException {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("password.xml"));
+            int lines = 0;
+            while (reader.readLine() != null) {
+                lines++;
+            }
+            reader.close();
+            int id = (lines - 3) / 5;
+            String xml =
+                    "\t<data " + "id=\"" + id + "\">\n" +
+                            "\t\t<textField>" + userText + "</textField>\n" +
+                            "\t\t<cipher_text>" + userTextCrypt + "</cipher_text>\n" +
+                            "\t\t<date_time>" + Calendar.getInstance().getTime() + "</date_time>" +
+                            "\t</data>\n";
+
+            return xml;
+        }
+        catch (Exception e) {
+
+            String xml =
+                    "\t<data id=\"1\">\n" +
+                            "\t\t<textField>" + userText + "</textField>\n" +
+                            "\t\t<cipher_text>" + userTextCrypt + "</cipher_text>\n" +
+                            "\t\t<date_time>" + Calendar.getInstance().getTime() + "</date_time>" +
+                            "\t</data>\n";
+
+            return xml;
+        }
+
+        }
+
+    private void writeToFile(String data, Context context) {
+        try
+        {
+            String filename= "password.xml";
+            FileWriter fw = new FileWriter(filename,true); //t
+            RandomAccessFile raf = new RandomAccessFile("password.xml", "rw");
+            long length = raf.length();
+            raf.setLength(length - 1);
+            raf.close();
+            fw.write(data + "</content_file>");
+            fw.close();
+        }
+        catch(IOException ioe) {
+
+            try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("password.xml", Context.MODE_PRIVATE));
+            outputStreamWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                    "<content_file>\n" + data +"</content_file>");
+            outputStreamWriter.close();
+            }
+            catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+            }
+        }
 
     }
+
+
 }
