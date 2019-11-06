da160212cad2f94b256282d2545e440f0a48a08a
Gastón Rossi <gaston.rossi@basetis.com>
2019-11-06 14:50:35 +0100

    Encrypt decrypt method

--- app/src/main/java/com/example/xifratge/MainActivity.java
+++ app/src/main/java/com/example/xifratge/MainActivity.java
@@ -21,6 +21,45 @@ public class MainActivity extends AppCompatActivity {
         this.textEncoded = findViewById(R.id.editText);
         this.textDecoded = findViewById(R.id.textView);
         this.decodeButton = findViewById(R.id.button);
+        decodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                encryptDecrypt(view);
+            }
+        });
+
+    }
+
+    public void encryptDecrypt(View view) {
+
+        try {
+            String password = this.textField.getText().toString();
+            String encodedText;
+            String decodedText;
+            RSA encodeRsa = new RSA();
+            RSA decodeRsa = new RSA();
+
+            encodeRsa.genKeyPair(1024);
+
+            encodeRsa.setContext(getBaseContext());
+            encodeRsa.saveToDiskPrivateKey("privateKey");
+            encodeRsa.saveToDiskPublicKey("publicKey");
+
+            decodeRsa.setContext(getBaseContext());
+            decodeRsa.openFromDiskPrivateKey("privateKey");
+            decodeRsa.openFromDiskPublicKey("publicKey");
+
+            encodedText = encodeRsa.Encrypt(password);
+            textDecoded.setText(encodedText);
+
+            decodedText = decodeRsa.Decrypt(encodedText);
+            textEncoded.setText(decodedText);
+
+
+        } catch (Exception e) {
+            System.out.println("An error ocurred encrypting the password");
+        }
+
 
     }
 }
